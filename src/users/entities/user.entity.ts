d544a3573668d29f375f29bf9e241366b70a9216
Benevanio Santos <benevaniosantos930@gmail.com>
2026-05-07 19:31:02 -0300

    feat: define UserProps type and implement User class with properties and methods

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+export type UserProps = {
+  id: string
+  name: string
+  email: string
+  password: string
+}
+export class User {
+  constructor(public props: UserProps) {}
+
+  get id() {
+    return this.props.id
+  }
+  get name() {
+    return this.props.name
+  }
+  get email() {
+    return this.props.email
+  }
+  get password() {
+    return this.props.password
+  }
+  toJSON() {
+    return {
+      id: this.id,
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    }
+  }
+}
