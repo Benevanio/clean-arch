d09dc544cdcafff8d601de5535bbf0b4bfb992e6
Benevanio Santos <benevaniosantos930@gmail.com>
2026-05-11 21:02:12 -0300

    feat: test

--- src/users/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/entities/__tests__/unit/user.entity.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { faker } from '@faker-js/faker'
 import { User } from '../../user.entity'
