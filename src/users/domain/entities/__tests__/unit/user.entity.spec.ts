59438702b8891077c16eb11f81b39375c79a73c2
Benevanio Santos <benevaniosantos930@gmail.com>
2026-05-19 19:22:50 -0300

    feat(tests): criando  o teste para o user email

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
-import { User } from '../../user.entity'
+import { User, UserEntity } from '../../user.entity'
 
 describe('UserEntity', () => {
   beforeEach(() => {
@@ -48,5 +48,24 @@ describe('UserEntity', () => {
         createdAt: props.createdAt,
       },
     })
+  })
+
+  it("Get Name Field should return the user's name", () => {
+    const props = UserDataBuilder({})
+
+    const userEntity = new UserEntity(props)
+
+    expect(userEntity.name).toBe(props.name)
+  })
+  it("Setter Name Field should update the user's name", () => {
+    const props = UserDataBuilder({})
+
+    const userEntity = new UserEntity(props)
+
+    const newName = 'New Name'
+
+    userEntity.update(newName)
+
+    expect(userEntity.name).toBe(newName)
   })
 })
